1d20231d7627a8d3442df89f6423c660e5a2f1e6
neeratipooja <116306537+neeratipooja@users.noreply.github.com>
2022-11-22 21:36:41 +0530

    Add files via upload

---
+++ strings1.py
@@ -0,0 +1,14 @@
+print('hi')
+print('bye')
+print('')
+print('friend  m ')
+
+print('hi','jry')
+print('',"hru")
+a=4
+print(str('a'+'b'))
+a=4
+print(str(a)+'+b')
+print(type('4'))
+
+
---
+++ types1.py
@@ -0,0 +1,12 @@
+'''
+DATE=20TH NOV 2022
+DAY= Sunday
+TOPIC= TYPES
+AUTHOR= pooja
+'''
+a=4
+print(type(a)) #int
+b=2.4
+print(type(b)) #float
+c='5'
+print(type(c)) #string
